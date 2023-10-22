35fdf03f43be9b7bf7655f94e2d792345564a90c
Shin'ichi Konomi <konomi@acm.org>
2023-10-23 08:49:18 +0900

    Add files via upload

---
+++ chatapp.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import openai
+
+st.title("Chat Appp (GPT-4)")
+
+# Set OpenAI API key from Streamlit secrets
+openai.api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+if not openai.api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+else:
+    for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
